405ed3e46685daf31e2d8b147528977f54a7e2ac
Xiu Wen <xiuwenseow@yahoo.com.sg>
2022-05-11 16:53:33 +0800

    Add login and signup forms
    Remaining vertical center issue.

--- mini-proj/config/routes.rb
+++ mini-proj/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
 
   devise_scope :user do 
-    get 'login', to: 'devise/sessions#new'
-    get 'signup', to: 'devise/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    get 'signout', to: 'users/sessions#destroy'
+    get 'signup', to: 'users/registrations#new'
   end
   
   root 'static_pages#home'
