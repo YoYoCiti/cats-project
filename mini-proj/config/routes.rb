71b69cbd019fabf3e7a0bbed64c938ef84cfd1d0
Xiu Wen <xiuwenseow@yahoo.com.sg>
2022-05-11 14:32:17 +0800

    Set up devise for authentication
    Devise configuration completed. Login and sign up routes set up.

--- mini-proj/config/routes.rb
+++ mini-proj/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do 
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
   root 'static_pages#home'
 end
