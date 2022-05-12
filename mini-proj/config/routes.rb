38ca066937bce2f9aef067acf4291ccfb31aede6
Xiu Wen <xiuwenseow@yahoo.com.sg>
2022-05-12 13:53:56 +0800

    Delete devise controllers

--- mini-proj/config/routes.rb
+++ mini-proj/config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
 
   devise_scope :user do 
-    get 'login', to: 'users/sessions#new'
-    get 'signout', to: 'users/sessions#destroy'
-    get 'signup', to: 'users/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+    get 'signout', to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
   end
   
   root 'static_pages#home'
