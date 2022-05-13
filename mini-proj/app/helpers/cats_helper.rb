3480fa68458dea98e914817c3e21fa3bc970598b
Xiu Wen <xiuwenseow@yahoo.com.sg>
2022-05-13 16:38:50 +0800

    Add font-awesome icons for cat profile

--- mini-proj/app/helpers/cats_helper.rb
+++ mini-proj/app/helpers/cats_helper.rb
@@ -1,2 +1,10 @@
 module CatsHelper
+    def gender_icon(gender)
+        case gender 
+        when 'female' 
+            'venus'
+        when 'male'
+            'mars'
+        end
+    end
 end
