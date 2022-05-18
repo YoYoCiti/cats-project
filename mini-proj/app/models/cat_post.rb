054de89e5f915c8abade25674cc2d1e05a6d9922
Xiu Wen <xiuwenseow@yahoo.com.sg>
2022-05-18 21:26:05 +0800

    Fix cat post carousel bug
    Next button does not control only one carousel anymore. Ordered cat posts by creation date.

--- mini-proj/app/models/cat_post.rb
+++ mini-proj/app/models/cat_post.rb
@@ -3,6 +3,8 @@ class CatPost < ApplicationRecord
   belongs_to :cat
   has_many_attached :images
 
+  default_scope -> { order(created_at: :desc) }
+
   validates :user_id, presence: true
   validates :cat_id, presence: true 
   validates :content, presence: true
